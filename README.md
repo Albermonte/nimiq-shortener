3c47ad4573d442b9e466845faa19a3b8ec669008
alber monte <alberto.monterroso@hotmail.com>
2018-08-19 20:18:11 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple URL Shortener
+
+Source Code of the Code from the article of [Adventure Of Palash](https://palash.tk)
+
+Tools Using : 
+
+- jsonstore.io
+- html
+- javascript
+- jquery
+
+No Need Of Database !
+
+READ THE Original ARTICLE HERE
+
+[![Blog Post](https://palash.tk/assets/images/build_url_shortener.png)](https://palash.tk/Build-URL-Shortener-With-HTML-JS)
---
+++ main.js
@@ -0,0 +1,58 @@
+var endpoint = "https://www.jsonstore.io/394221034bda266cb95edd70c9932b3b930aaa60628853dc31131f864cbd835c";
+
+function geturl(){
+    var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if(!protocol_ok){
+        newurl = "http://"+url;
+        return newurl;
+        }else{
+            return url;
+        }
+}
+
+function getrandom() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+function genhash(){
+    if (window.location.hash == ""){
+        window.location.hash = getrandom();
+    }
+}
+
+function send_request(url) {
+    this.url = url;
+    $.ajax({
+        'url': endpoint + "/" + window.location.hash.substr(1),
+        'type': 'POST',
+        'data': JSON.stringify(this.url),
+        'dataType': 'json',
+        'contentType': 'application/json; charset=utf-8'
+})
+}
+
+function shorturl(){
+    var longurl = geturl();
+    genhash();
+    send_request(longurl);
+}
+
+var hashh = window.location.hash.substr(1)
+
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + hashh, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+        }
+
+    });
+}
+
