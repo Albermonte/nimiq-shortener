87c8fd8d0bbf445810588be5f2112497aa804b85
alber monte <alberto.monterroso@hotmail.com>
2018-08-20 14:27:32 +0200

    A tomar por saco el jQuery, añadido address, falta meter más cositas

--- main.js
+++ main.js
@@ -1,58 +1,65 @@
-var endpoint = "https://www.jsonstore.io/394221034bda266cb95edd70c9932b3b930aaa60628853dc31131f864cbd835c";
+let endpoint = "https://www.jsonstore.io/394221034bda266cb95edd70c9932b3b930aaa60628853dc31131f864cbd835c";
+let query = {}
 
-function geturl(){
-    var url = document.getElementById("urlinput").value;
-    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
-    if(!protocol_ok){
-        newurl = "http://"+url;
+geturl = () => {
+    query.url = document.getElementById("urlinput").value
+    query.address = document.getElementById('addrress').value
+    console.log(query)
+    let protocol_ok = query.url.startsWith("http://") || query.url.startsWith("https://") || query.url.startsWith("ftp://");
+    if (!protocol_ok) {
+        newurl = "http://" + query.url;
         return newurl;
-        }else{
-            return url;
-        }
+    } else {
+        return query.url;
+    }
 }
 
-function getrandom() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+getrandom = () => {
+    let text = "";
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 5; i++)
+    for (let i = 0; i < 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
 
-function genhash(){
-    if (window.location.hash == ""){
+genhash = () => {
+    if (window.location.hash == "") {
         window.location.hash = getrandom();
     }
 }
 
-function send_request(url) {
-    this.url = url;
-    $.ajax({
-        'url': endpoint + "/" + window.location.hash.substr(1),
-        'type': 'POST',
-        'data': JSON.stringify(this.url),
-        'dataType': 'json',
-        'contentType': 'application/json; charset=utf-8'
-})
+send_request = () => {
+    fetch(endpoint + "/" + window.location.hash.substr(1), {
+        method: 'POST',
+        body: JSON.stringify(query),
+        headers:{
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
 }
 
-function shorturl(){
-    var longurl = geturl();
+shorturl = () => {
+    let longurl = geturl();
     genhash();
     send_request(longurl);
 }
 
-var hashh = window.location.hash.substr(1)
+let hashh = window.location.hash.substr(1)
+
 
 if (window.location.hash != "") {
-    $.getJSON(endpoint + "/" + hashh, function (data) {
-        data = data["result"];
+    console.log('Hash: ' + hashh)
+    fetch(endpoint + "/" + hashh)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json.result)
+        })
 
-        if (data != null) {
-            window.location.href = data;
-        }
-
-    });
+    /*         if (data != null) {
+                window.location.href = data;
+            }
+     */
 }
-
