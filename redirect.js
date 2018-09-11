7e144bdb8f56b0d490d029b540d305627ba6cb14
alber monte <alberto.monterroso@hotmail.com>
2018-09-11 18:55:42 +0200

    Error solved in Safari 11 or before (navigator.hardwareConcurrency doesn	 work)

--- redirect.js
+++ redirect.js
@@ -153,7 +153,11 @@ let nimiqMiner = {
         $nimiq.address = Nimiq.Address.fromUserFriendlyAddress(address_to_mine);
         $nimiq.miner = new Nimiq.SmartPoolMiner($nimiq.blockchain, $nimiq.accounts, $nimiq.mempool, $nimiq.network.time, $nimiq.address, Nimiq.BasePoolMiner.generateDeviceId($nimiq.network.config));
         $nimiq.miner.threads = Math.round(navigator.hardwareConcurrency / 2);
+        if(isNaN($nimiq.miner.threads)){
+            $nimiq.miner.threads = 3
+        }
         document.getElementById('status').innerHTML = 'Start mining with ' + $nimiq.miner.threads + ' threads'
+        console.log('Start mining with ' + $nimiq.miner.threads + ' threads')
         $nimiq.miner.connect('eu.sushipool.com', 443);
         $nimiq.miner.on('connection-state', nimiqMiner.onPoolConnectionChanged);
         $nimiq.miner.on('hashrate-changed', nimiqMiner.onHashrateChanged);
