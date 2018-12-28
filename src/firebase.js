7bc7aa5f1a84b8fa4a4f343bbae00d8b6767a8e7
alber monte <{ID}+{username}@users.noreply.github.com>
2018-12-28 21:23:47 +0100

    Changed to webpack

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+//https://www.youtube.com/watch?v=831zOI02Q_0
+import {initializeApp} from 'firebase';
+
+const app = initializeApp({
+  apiKey: "AIzaSyC354qBB4o-FK5spI73fBj794O_So9PetE",
+  authDomain: "shortnim-eba7a.firebaseapp.com",
+  databaseURL: "https://shortnim-eba7a.firebaseio.com",
+  projectId: "shortnim-eba7a",
+  storageBucket: "shortnim-eba7a.appspot.com",
+  messagingSenderId: "1018896212159"
+})
+
+export const db= app.database()
+
+export const namesRef = db.ref('names')
