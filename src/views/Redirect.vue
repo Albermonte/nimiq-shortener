c15e4b6e6085f73415cf57148bb31e8013c33bb4
Albermonte <{ID}+{username}@users.noreply.github.com>
2019-01-06 13:57:12 +0100

    No time for good message

--- src/views/Redirect.vue
+++ src/views/Redirect.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-//import { namesRef } from "../firebase.js";
+import { namesRef } from "../firebase.js";
 
 const $nimiq = {};
 
@@ -13,40 +13,45 @@ export default {
   name: "redirect",
   data() {
     return {
-      id: ""
+      id: "",
+      address: "NQ65 GS91 H8CS QFAN 1EVS UK3G X7PL L9N1 X4KC",
+      shares: 0
     };
   },
   mounted() {
-    console.log("mounted");
     if (this.$route.params.id != null) {
+      this.id = this.$route.params.id;
       console.log(this.$route.params.id);
     } else {
       console.log("Bad id");
     }
-    const vm = this;
-    // setInterval(() => {
-    //   if ($nimiq && $nimiq.miner) {
-    //     vm.hashrate = $nimiq.miner._hashrate.toString();
-    //     vm.height = $nimiq.blockchain.height.toString();
-    //   }
-    // }, 1000);
-    // setTimeout(() => {
-    //   if (!vm.firstExpand) {
-    //     vm.expanded = true;
-    //   }
-    // }, 5000);
+
+    namesRef.child(`${this.id}/address`).once("value", function(data) {
+      if (data.val() == null) {
+        console.log("Can't find ID");
+      } else {
+        this.address = data.val();
+      }
+    });
+    namesRef.child(`${this.id}/shares`).once("value", function(data) {
+      if (data.val() == null) {
+        console.log("Can't find ID");
+      } else {
+        this.shares = data.val();
+      }
+    });
+
+    const _this = this;
     Nimiq.init(
       async function() {
-        vm.status = "Syncing";
+        _this.status = "Syncing";
         Nimiq.GenesisConfig.main();
         $nimiq.consensus = await Nimiq.Consensus.nano();
         $nimiq.blockchain = $nimiq.consensus.blockchain;
         $nimiq.network = $nimiq.consensus.network;
         $nimiq.accounts = $nimiq.blockchain.accounts;
         $nimiq.mempool = $nimiq.consensus.mempool;
-        const address = Nimiq.Address.fromUserFriendlyAddress(
-          "NQ65 GS91 H8CS QFAN 1EVS UK3G X7PL L9N1 X4KC"
-        );
+        const address = Nimiq.Address.fromUserFriendlyAddress(_this.address);
         const deviceId = Nimiq.BasePoolMiner.generateDeviceId(
           $nimiq.network.config
         );
@@ -58,24 +63,75 @@ export default {
         );
         $nimiq.miner.threads = 1;
 
-        $nimiq.miner.on("connection-state", () => {});
+        $nimiq.miner.on("share", () => {
+          _this.shares_mined++;
+          console.log(_this.shares_mined)
+          namesRef
+            .child(`${this.id}/shares_mined`)
+            .once("value", function(data) {
+              let shares = data.val();
+              shares++
+              namesRef.child(`${this.id}`).set({
+                shares_mined: shares
+              });
+            });
+
+          if (_this.shares_mined >= _this.shares) {
+            namesRef.child(`${this.id}/url`).once("value", function(data) {
+              if (data.val() == null) {
+                console.log("Can't find ID");
+              } else {
+                window.location.href = data.val();
+              }
+            });
+          }
+        });
+
+        $nimiq.miner.on("connection-state", state => {
+          if (state === Nimiq.BasePoolMiner.ConnectionState.CONNECTING) {
+            console.log("Connecting to the pool");
+          }
+          if (state === Nimiq.BasePoolMiner.ConnectionState.CONNECTED) {
+            console.log("Connected to pool");
+            console.log("Mining");
+            
+            $nimiq.miner.startWork();
+            let hack = setInterval(() => {
+              if (!$nimiq.miner._shouldWork) {
+                console.log("Pls fix")
+                $nimiq.miner.disconnect();
+                setTimeout(() => {
+                  $nimiq.miner.connect(
+                    "eu.nimpool.io",
+                    8444
+                  );
+                }, 1000);
+              }else{
+                clearInterval(hack)
+              }
+            }, 3000);
+            
+          }
+          if (state === Nimiq.BasePoolMiner.ConnectionState.CLOSED) {
+            console.log("Connection closed");
+          }
+        });
+
+        $nimiq.miner.on("hashrate-changed", rate => {
+          console.log(rate);
+        });
+
         $nimiq.consensus.on("established", () => {
-          vm.status = "Established";
-          console.log(vm.status);
+          _this.status = "Established";
+          console.log(_this.status);
 
           $nimiq.miner.connect(
             "eu.nimpool.io",
             8444
           );
-          console.log("Mining");
-          $nimiq.miner.startWork();
-          setInterval(
-            console.log("Hashrate: " + $nimiq.miner._hashrate.toString()),
-            350
-          );
         });
         $nimiq.consensus.on("lost", () => {
-          vm.status = "Lost";
+          _this.status = "Lost";
         });
         $nimiq.network.connect();
       },
@@ -86,16 +142,7 @@ export default {
   },
   methods: {
     /*
-    namesRef.child(customID).once("value", function(data) {
-        if (data.val() == null) {
-          _this.submitID(customID);
-          console.log("Done");
-        } else {
-          console.log("Again");
-          _this.generateID();
-        }
-      });
-    */
+     */
   }
 };
 </script>
